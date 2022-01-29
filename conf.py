b1fcb90c2f431542bb196a70ccf5b2fc02d2be71
Zach Coriarty <zackcor27@gmail.com>
2022-01-29 14:33:19 -0500

    css stylesheet

--- conf.py
+++ conf.py
@@ -16,6 +16,20 @@ nbsphinx_allow_errors = True
 
 html_sourcelink_suffix = ''
 
+html_static_path = ['_static']
+html_css_files = ['my-own-style.css']
+
+nbsphinx_prolog = """
+.. raw:: html
+
+    <style>
+        h1 {
+            color: chartreuse;
+        }
+    </style>
+"""
+
+
 source_suffix = [".rst", ".md", ".ipynb"]
 
 
